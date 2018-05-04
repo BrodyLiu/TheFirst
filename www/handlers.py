93668a4206d75b70c515c35e57856b510764c140
ldy <doing_be@qq.com>
2018-05-04 19:10:48 +0800

    day 8

--- www/handlers.py
+++ www/handlers.py
@@ -1,11 +1,18 @@
-from models import User
+from models import User, Blog
 from coroweb import get
+import time
 
 
 @get('/')
 async def index(request):
+    summary = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    blogs = [
+        Blog(id='1', name='Test Blog', summary=summary, created_at=time.time()-120),
+        Blog(id='2', name='Something New', summary=summary, created_at=time.time()-3600),
+        Blog(id='3', name='Learn Swift', summary=summary, created_at=time.time()-7200)
+    ]
     users = await User.findAll()
     return {
-        '__template__':'test.html',
-        'users':users
+        '__template__':'blog.html',
+        'blogs':blogs
     }
