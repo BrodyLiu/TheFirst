e083d88d28e503162882deacdbe566e1590dc521
ldy <doing_be@qq.com>
2018-05-08 20:43:15 +0800

    day 10

--- www/handlers.py
+++ www/handlers.py
@@ -1,6 +1,14 @@
-from models import User, Blog
-from coroweb import get
-import time
+import re, time, json, logging, hashlib, base64, asyncio
+
+import markdown2
+
+from aiohttp import web
+
+from coroweb import get, post
+from apis import APIValueError, APIResourceNotFoundError
+
+from models import User, Comment, Blog, next_id
+from config import configs
 
 
 @get('/')
@@ -16,3 +24,30 @@ async def index(request):
         '__template__':'blog.html',
         'blogs':blogs
     }
+
+
+@get('/api/users')
+async def api_get_users():
+    users = await User.findAll(orderBy='created_at desc')
+    for u in users:
+        u.passwd = '******'
+    return dict(users = users)
+
+__RE_EMAIL = re.compile(r'^[a-z0-9\.\-\_]+\@[a-z0-9\-\_]+(\.[a-z0-9\-\_]+){1,4}$')
+__RE_SHA1 = re.compile(r'^[0-9a-f]{40}$')
+
+
+@post('/api/users')
+async def api_register_user(*, email, name, passwd):
+    if not name or not name.strip():
+        raise APIValueError('name')
+    if not email or not __RE_EMAIL.match(email):
+        raise APIValueError('email')
+    if not passwd or not __RE_SHA1.match(passwd):
+        raise APIValueError('passwd')
+    users = await User.findAll('email = ?', [email])
+    if len(users) > 0:
+        raise APIValueError('register:failed', 'email', 'email is already in use.')
+    uid = next_id()
+
+
